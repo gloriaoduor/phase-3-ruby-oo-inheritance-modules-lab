13f20d2cfb45a23ebf0723aed29989c452a9e76d
gloria oduor <gloriaoduor@gmail.com>
2022-08-24 20:54:46 +0300

    Done.

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,14 @@
+module Memorable
+  module ClassMethods
+    def self.reset_all
+      self.all.clear
+    end
+    def self.count
+      self.all.count
+    end
+  module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
+  end
+end
